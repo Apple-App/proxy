82b7faabdc992568188b5824f775c8c18eadef65
Nick <nbisigna@gmail.com>
2019-01-16 14:26:14 -0500

    env variables

--- server.js
+++ server.js
@@ -1,44 +1,40 @@
-const express = require("express");
-const request = require("request");
+const express = require('express');
+const request = require('request');
 const app = express();
 
 app.use(express.static(__dirname));
 
 app.use((req, res, next) => {
-  let url = req.url.split("/");
-  let path = url.slice(2).join("/");
+  let url = req.url.split('/');
+  let path = url.slice(2).join('/');
   let endpoint = url[1];
-  if (endpoint === "main") {
-    let proxy =
-      "http://ec2-3-17-128-193.us-east-2.compute.amazonaws.com/" + path;
+  if (endpoint === 'main') {
+    let proxy = process.env.MAIN_URL + path;
     request(proxy)
-      .on("error", err => console.log(err))
+      .on('error', err => console.log(err))
       .pipe(res);
-  } else if (endpoint === "details") {
-    let proxy =
-      "http://ec2-18-218-63-15.us-east-2.compute.amazonaws.com/" + path;
-
+  } else if (endpoint === 'details') {
+    let proxy = process.env.DETAILS_URL + path;
     request(proxy)
-      .on("error", err => console.log(err))
+      .on('error', err => console.log(err))
       .pipe(res);
-  } else if (endpoint === "critics") {
-    let proxy =
-      "http://ec2-3-16-138-173.us-east-2.compute.amazonaws.com/" + path;
+  } else if (endpoint === 'critics') {
+    let proxy = process.env.CRITICS_URL + path;
     request(proxy)
-      .on("error", err => console.log(err))
+      .on('error', err => console.log(err))
       .pipe(res);
-  } else if (endpoint === "sidebar") {
-    if (path.includes("images")) {
-      path = "sidebar/" + path;
+  } else if (endpoint === 'sidebar') {
+    if (path.includes('images')) {
+      path = 'sidebar/' + path;
     }
-    let proxy = "http://18.222.207.221:9004/" + path;
+    let proxy = process.env.SIDEBAR_URL + path;
     request(proxy)
-      .on("error", err => console.log(err))
+      .on('error', err => console.log(err))
       .pipe(res);
   }
 });
 
-app.get("/", (req, res) => {});
+app.get('/', (req, res) => {});
 
 let port = process.env.PORT || 3000;
-app.listen(port, () => console.log("Server listening on port " + port));
+app.listen(port, () => console.log('Server listening on port ' + port));
